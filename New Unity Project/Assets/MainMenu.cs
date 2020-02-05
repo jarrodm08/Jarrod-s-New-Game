a597ebf3dbc73ea28e505810f12e6d7438b161c1
Jarrod Moyer <jarrodw9@gmail.com>
2020-02-05 07:27:34 -0600

    Cloud Scrolling
    Simplified a code to loop two images of clouds seemlessly to allow a "infinite" scrolling effect. Adjustable cloud speed is in the script.

--- New Unity Project/Assets/MainMenu.cs
+++ New Unity Project/Assets/MainMenu.cs
@@ -4,60 +4,37 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-
-    // Start is called before the first frame update
+ 
     void Start()
     {
         loadClouds();
-        //GameObject newCloud = Instantiate(cloudPrefab,cloudSpawnA.transform.position,cloudSpawnA.transform.rotation,cloudSpawnA.transform);
     }
 
-    // Update is called once per frame
     void Update()
     {
-
+        moveClouds();
     }
 
+    
+   
 
 
-    private GameObject cloudPrefab;
-    private GameObject spawnA;
-    private GameObject spawnB;
-    Dictionary<Sprite, GameObject> cloudDic = new Dictionary<Sprite, GameObject>();
-    Dictionary<int, Dictionary<Sprite, GameObject>> cloudSpawnsDic = new Dictionary<int, Dictionary<Sprite, GameObject>>();
-    //Dictionary<Sprite, GameObject> cloudDic = new Dictionary<Sprite, GameObject>();
+
+    // CLOUDS //
+    private GameObject cloudA;
+    private Vector2 startPos;
+    private float scrollSpeed = 3f;
 
     private void loadClouds()
     {
-        Sprite[] cloudSprites = Resources.LoadAll<Sprite>("Art/Main Menu/Clouds"); // Load cloud sprites
-        spawnA = this.transform.parent.Find("CloudSpawnA").Find("Spawn").gameObject;
-        spawnB = this.transform.parent.Find("CloudSpawnB").Find("Spawn").gameObject;
-        cloudPrefab = Resources.Load<GameObject>("Art/Main Menu/Cloud");
-
-
-
-        cloudDic.Add(cloudSprites[0],spawnB);
-        cloudDic.Add(cloudSprites[1],spawnA);
-        cloudDic.Add(cloudSprites[3],spawnB);
-        cloudDic.Add(cloudSprites[2],spawnA);
-
-        int cloudNum = 0;
-        foreach (KeyValuePair<Sprite,GameObject> p in cloudDic)
-        {
-            cloudNum++;
-            cloudSpawnsDic.Add(cloudNum,p);
-        }
-
-        spawnCloud();
+        cloudA = this.transform.parent.Find("Clouds").gameObject;
+        startPos = cloudA.transform.position;
     }
 
-    private int cloudNum = 0;
-
-    public void spawnCloud()
+    private void moveClouds()
     {
-        foreach (KeyValuePair<int,Dictionary<Sprite,GameObject>> p in cloudSpawnsDic)
-        {
-            Debug.Log("Cloud #"+ p.Key + " has sprite: " + p.Value.);
-        }
+        float newPos = Mathf.Repeat(Time.time * scrollSpeed,17.7f); // Number 17.7f depends on size of image, adjust to a precise number where the image repeats seemlessly
+        cloudA.transform.position = startPos + Vector2.right * newPos;
     }
+    // ----- //
 }
